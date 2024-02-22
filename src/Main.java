89071a02d709d350084bb5c7c5fb96d1c1f408de
Ar1sAlan1s <arisalanis468@gmail.com>
2024-02-22 10:49:54 -0600

    Tarea Pareja

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int numa = 0, x;
+        boolean band = false;
+
+        Random ran = new Random();
+        numa = ran.nextInt(11);
+        Scanner leer = new Scanner(System.in);
+        for (; band == false; ) {
+            System.out.print("Adivina el numero: ");
+            System.out.println(numa);
+            x = leer.nextInt();
+            if (x == numa) {
+                band = true;
+            }
+
+        }
+    }
+}
